8027b7372c746c43a1c54b9089c648b4c2e282ee
kimi.casamina <kimi.casamina@gmail.com>
2024-09-19 08:38:21 +0800

    installed react-icons, build files, and updated the README.md

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: false,
   },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
   habits: [
     {
       type: mongoose.Schema.Types.ObjectId,
