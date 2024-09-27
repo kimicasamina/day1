e52475f613b1911c93af567d135e4842720d801f
kimi.casamina <kimi.casamina@gmail.com>
2024-09-27 17:46:47 +0800

    Fixed minor bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,31 @@ import { ErrorBoundary } from "@sentry/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import toast, { Toaster } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { getTags } from "@/store/tags/actions";
+import { getHabits } from "@/store/habits/actions";
+import { useEffect } from "react";
+import { useAuth } from "./context/auth/auth";
 
-import axios from "axios";
 function App() {
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        dispatch(getTags());
+        dispatch(getHabits());
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
+
   return (
     <ErrorBoundary fallback={"An error has occurred"}>
       <Toaster />
