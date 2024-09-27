7252e90f291b09864ce015d6deb6b5e007966647
kimi.casamina <kimi.casamina@gmail.com>
2024-09-27 16:12:46 +0800

    FEAT: fetching initial data at the profile layout

--- frontend/src/layout/ProfileLayout.jsx
+++ frontend/src/layout/ProfileLayout.jsx
@@ -2,9 +2,26 @@ import React, { useState, useEffect } from "react";
 import Nav from "../components/nav/Nav";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "@/context/auth/auth";
+import { useDispatch } from "react-redux";
+import { getTags } from "@/store/tags/actions";
+import { getHabits } from "@/store/habits/actions";
 
 export default function ProfileLayout() {
   const { user, isFetching, error } = useAuth();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        dispatch(getTags());
+        dispatch(getHabits());
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   console.log("USER: ", user);
 
