da392693b1f8404dc7d3dbb4b32502d067753a38
kimi.casamina <kimi.casamina@gmail.com>
2024-09-27 12:22:01 +0800

    fixed: removed habit list scrollbar

--- frontend/src/features/habits/components/habitList/index.jsx
+++ frontend/src/features/habits/components/habitList/index.jsx
@@ -5,7 +5,7 @@ export default function HabitList({ habits, searchResults }) {
   if (!habits) return <h1 className="">Loading...</h1>;
 
   return (
-    <ul className="habits__list h-full overflow-y-scroll flex flex-col gap-y-2 ">
+    <ul className="habits__list h-full overflow-y-scroll no-scrollbar flex flex-col gap-y-2 ">
       {searchResults
         ? searchResults.map((habit) => <Habit habit={habit} key={habit._id} />)
         : null}
