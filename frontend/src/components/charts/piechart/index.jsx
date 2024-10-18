162af7b5f570882622720d42c6510094ad6f9c6e
kimi.casamina <kimi.casamina@gmail.com>
2024-10-18 20:22:50 +0800

    removed chart dep and components

--- frontend/src/components/charts/piechart/index.jsx
+++ frontend/src/components/charts/piechart/index.jsx
@@ -1,30 +1,30 @@
 // import { Pie } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+// ChartJS.register(ArcElement, Tooltip, Legend);
 
-const TotalTodoPieChart = () => {
-  const data = {
-    labels: ["Fitness", "Career", "Productivity", "Social", "Health"],
-    datasets: [
-      {
-        data: [12, 29, 32, 11, 14],
-        backgroundColor: [
-          "rgb(87,169,174)",
-          "rgb(122,202,206)",
-          "rgb(104, 245, 247)",
-          "rgb(57, 212, 215) ",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+// const TotalTodoPieChart = () => {
+//   const data = {
+//     labels: ["Fitness", "Career", "Productivity", "Social", "Health"],
+//     datasets: [
+//       {
+//         data: [12, 29, 32, 11, 14],
+//         backgroundColor: [
+//           "rgb(87,169,174)",
+//           "rgb(122,202,206)",
+//           "rgb(104, 245, 247)",
+//           "rgb(57, 212, 215) ",
+//         ],
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
 
-  return (
-    <div className="w-full h-full">
-      <Pie data={data} />
-    </div>
-  );
-};
+//   return (
+//     <div className="w-full h-full">
+//       <Pie data={data} />
+//     </div>
+//   );
+// };
 
-export default TotalTodoPieChart;
+// export default TotalTodoPieChart;
