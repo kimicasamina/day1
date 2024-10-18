7675c8ea9a43c7bc5eebc42a8b4915580e8ac508
kimi.casamina <kimi.casamina@gmail.com>
2024-10-18 19:41:27 +0800

    hide piechart

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -6,11 +6,11 @@ export default function Dashboard() {
   return (
     <div className="min-h-full w-full max-w-[1440px] mx-auto px-4 flex flex-col gap-8 bg-primary-foreground overflow-y-scroll no-scrollbar">
       {/* <div className="w-full h-[50%] grid grid-cols-1 md:grid-cols-2 gap-x-4 justify-between">
-        <div className="flex-1 w-full h-full flex flex-col justify-center"> 
+        <div className="w-full h-full flex flex-col justify-center">
           <h1 className="font-semibold text-3xl">Todos Overview</h1>
           <Piechart />
         </div>
-        <div className="flex-1 w-full h-full flex flex-col justify-center">
+        <div className="w-full h-full flex flex-col justify-center">
           <h1 className="font-semibold text-3xl">Goals Overview</h1>
           <Piechart />
         </div>
