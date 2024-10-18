eee5cf6587bce0481d065757b839273e1c2c06dc
kimi.casamina <kimi.casamina@gmail.com>
2024-10-18 20:07:41 +0800

    fixed package.json

--- frontend/src/components/charts/piechart/index.jsx
+++ frontend/src/components/charts/piechart/index.jsx
@@ -1,4 +1,4 @@
-import { Pie } from "react-chartjs-2";
+// import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
